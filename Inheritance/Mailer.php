70e7cbc2ec6f8643b47740f353d8914b2860fe83
rogermelich <rogermelich@gmail.com>
2015-10-04 12:15:58 +0200

    Afegit Arxiu Mother.php

---
+++ Inheritance/Mailer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roger
+ * Date: 4/10/15
+ * Time: 12:20
+ */
+
+class Mailer{
+
+    public function send($to, $from, $body){
+
+    }
+}
+
+
+class UserMailer extends Mailer {
+
+}
+
+
+class AdminMailer extends Mailer{
+
+}
